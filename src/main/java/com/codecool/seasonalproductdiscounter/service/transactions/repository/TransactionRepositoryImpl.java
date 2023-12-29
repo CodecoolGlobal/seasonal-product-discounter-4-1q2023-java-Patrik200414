fe50886b1c95fed3480b8444b5037194c75bde15
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-12-29 19:57:45 +0100

    Implementing the getAll method

--- src/main/java/com/codecool/seasonalproductdiscounter/service/transactions/repository/TransactionRepositoryImpl.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/service/transactions/repository/TransactionRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.codecool.seasonalproductdiscounter.service.transactions.repository;
 
+import com.codecool.seasonalproductdiscounter.model.enums.Color;
+import com.codecool.seasonalproductdiscounter.model.enums.Season;
+import com.codecool.seasonalproductdiscounter.model.products.Product;
 import com.codecool.seasonalproductdiscounter.model.transactions.Transaction;
+import com.codecool.seasonalproductdiscounter.model.users.User;
 import com.codecool.seasonalproductdiscounter.service.logger.Logger;
 import com.codecool.seasonalproductdiscounter.service.persistence.SqliteConnector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionRepositoryImpl implements TransactionRepository{
@@ -46,6 +50,58 @@ public class TransactionRepositoryImpl implements TransactionRepository{
 
     @Override
     public List<Transaction> getAll() {
-        return null;
+        String sqlQuery = "SELECT transactions.id AS transaction_id, date, price_paid, products.id AS product_id, products.name AS product_name, color, season, price, sold, users.id AS user_id, user_name, password" +
+            " FROM transactions INNER JOIN products ON transactions.product_id = products.id" +
+            " INNER JOIN users ON transactions.user_id = users.id;";
+
+        List<Transaction> transactions = new ArrayList<>();
+        Connection connection = sqliteConnector.getConnection();
+        try{
+            logger.logInfo("Collecting transactions from the database!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            addTransactions(resultSet, transactions);
+        } catch (SQLException e){
+            logger.logError(e.getMessage());
+        }
+        return transactions;
+    }
+
+    private void addTransactions(ResultSet resultSet, List<Transaction> transactions) throws SQLException {
+        while (resultSet.next()){
+            Product product = collectProductData(resultSet);
+            User user = collectUserData(resultSet);
+
+            Transaction transaction = collectTransactionData(resultSet, user, product);
+            transactions.add(transaction);
+        }
+    }
+
+    private Transaction collectTransactionData(ResultSet resultSet, User user, Product product) throws SQLException {
+        int transactionId = resultSet.getInt("transaction_id");
+        String[] transactionDate = resultSet.getString("date").split("-");
+        LocalDate date = LocalDate.of(Integer.parseInt(transactionDate[0]), Integer.parseInt(transactionDate[1]), Integer.parseInt(transactionDate[2]));
+        double pricePaid = resultSet.getDouble("price_paid");
+
+        return new Transaction(transactionId, date, user, product, pricePaid);
+    }
+
+    private User collectUserData(ResultSet resultSet) throws SQLException {
+        int userId = resultSet.getInt("user_id");
+        String userName = resultSet.getString("user_name");
+        String password = resultSet.getString("password");
+
+        return new  User(userId, userName, password);
+    }
+
+    private Product collectProductData(ResultSet resultSet) throws SQLException {
+        int productId = resultSet.getInt("product_id");
+        String productName = resultSet.getString("product_name");
+        Color color = Color.valueOf(resultSet.getString("color"));
+        Season season = Season.valueOf(resultSet.getString("season"));
+        double price = resultSet.getDouble("price");
+        boolean sold = resultSet.getInt("sold") == 1 ? true : false;
+
+        return new Product(productId, productName, color, season, price, sold);
     }
 }
