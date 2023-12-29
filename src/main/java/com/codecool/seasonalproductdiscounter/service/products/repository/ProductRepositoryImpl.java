745d3cba285604a50cf1095f6584e57a37e5bdd1
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-12-29 12:22:16 +0100

    Implementing the addProducts method

--- src/main/java/com/codecool/seasonalproductdiscounter/service/products/repository/ProductRepositoryImpl.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/service/products/repository/ProductRepositoryImpl.java
@@ -32,15 +32,19 @@ public class ProductRepositoryImpl implements ProductRepository{
             logger.logError(e.getMessage());
         }
 
+        avaibleProductsLogInfo(avaibleProducts);
+        return avaibleProducts;
+    }
+
+    private void avaibleProductsLogInfo(List<Product> avaibleProducts) {
         if(avaibleProducts.size() > 0){
             logger.logInfo("All available products have been collected (" + avaibleProducts.size() +")");
         } else{
             logger.logInfo("There is no available product in the database");
         }
-        return avaibleProducts;
     }
 
-    private static void collectAvaibleProducts(Connection connection, String sqlQuery, List<Product> avaibleProducts) throws SQLException {
+    private void collectAvaibleProducts(Connection connection, String sqlQuery, List<Product> avaibleProducts) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sqlQuery);
         while (resultSet.next()){
@@ -58,7 +62,50 @@ public class ProductRepositoryImpl implements ProductRepository{
 
     @Override
     public boolean addProducts(List<Product> products) {
-        return false;
+        String sqlQuery = "INSERT INTO products VALUES(?, ?, ?, ?, ?, ?)";
+        Connection connection = sqliteConnector.getConnection();
+        int successfullInsertionCount = 0;
+        try{
+            successfullInsertionCount = insertRecordIntoDatabase(products, connection, sqlQuery);
+        } catch (SQLException e){
+            logger.logError(e.getMessage());
+        }
+
+        return insertionSuccess(products, successfullInsertionCount);
+    }
+
+    private boolean insertionSuccess(List<Product> products, int successfullInsertionCount) {
+        if(successfullInsertionCount == products.size()){
+            logger.logInfo("Every product was successfully inserted into the database");
+            return true;
+        } else{
+            logger.logError("Something went wrong!" + (products.size() - successfullInsertionCount) + " record have been lost!\nPlease try to insert the records again!");
+            return false;
+        }
+    }
+
+    public int insertRecordIntoDatabase(List<Product> products, Connection connection, String sqlQuery) throws SQLException {
+        int successfullInsertionCount = 0;
+        for(Product product : products){
+            PreparedStatement preparedStatement = getPreparedStatementForInsertion(connection, sqlQuery, product);
+
+            if(preparedStatement.execute()){
+                successfullInsertionCount++;
+            }
+        }
+
+        return successfullInsertionCount;
+    }
+
+    private static PreparedStatement getPreparedStatementForInsertion(Connection connection, String sqlQuery, Product product) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+        preparedStatement.setInt(1, product.id());
+        preparedStatement.setString(2, product.name());
+        preparedStatement.setString(3, product.color().name());
+        preparedStatement.setString(4, product.season().name());
+        preparedStatement.setDouble(5, product.price());
+        preparedStatement.setInt(6, product.sold() ? 1 : 0);
+        return preparedStatement;
     }
 
     @Override
