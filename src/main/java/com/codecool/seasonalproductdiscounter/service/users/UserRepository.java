d4c018610a1621224240f716a1a40cc597b9c7b1
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-12-29 14:43:20 +0100

    Creating the UserRepository interface

--- src/main/java/com/codecool/seasonalproductdiscounter/service/users/UserRepository.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/service/users/UserRepository.java
@@ -1,4 +1,19 @@
 package com.codecool.seasonalproductdiscounter.service.users;
 
+import com.codecool.seasonalproductdiscounter.model.users.User;
+
+import java.util.List;
+
 public interface UserRepository {
+    /*
+    List<Product> getAvailableProducts();
+    boolean addProducts(List<Product> products);
+    boolean setProductAsSold(Product product);
+    * */
+
+    List<User> getUsers();
+    User getUserByUserName(String userName);
+    boolean addUser(User user);
+    boolean changeUserName(User user, String userName);
+    boolean changePassword(User user, String password);
 }
