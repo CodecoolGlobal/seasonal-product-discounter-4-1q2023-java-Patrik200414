46d6409372cb14b53cb5b5863e44204877fda12e
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-22 10:26:06 +0100

    Rename dbUrl

--- src/main/java/com/codecool/seasonalproductdiscounter/service/persistence/DatabaseConnection.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/service/persistence/DatabaseConnection.java
@@ -8,18 +8,18 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private final String dbFile;
+    private final String dbUrl;
     private final Logger logger;
 
     public DatabaseConnection(String dbFile, Logger logger) {
-        this.dbFile = dbFile;
+        this.dbUrl = dbFile;
         this.logger = logger;
     }
 
     public Connection getConnection() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(dbFile);
+            conn = DriverManager.getConnection(dbUrl);
         } catch (SQLException e) {
             logger.logError(e.getMessage());
         }
