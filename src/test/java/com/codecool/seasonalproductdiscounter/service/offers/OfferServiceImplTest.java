d12d7fc303728a1d87c78bfdd780b210940cec8f
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-23 15:43:16 +0100

    Add unit tests to ProductBrowserImpl

--- src/test/java/com/codecool/seasonalproductdiscounter/service/offers/OfferServiceImplTest.java
+++ src/test/java/com/codecool/seasonalproductdiscounter/service/offers/OfferServiceImplTest.java
@@ -1,4 +1,13 @@
+package com.codecool.seasonalproductdiscounter.service.offers;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OfferServiceImplTest {
-  
+
+    @Test
+    void getOffers() {
+        //OfferService offerService = new OfferServiceImpl()
+    }
 }
