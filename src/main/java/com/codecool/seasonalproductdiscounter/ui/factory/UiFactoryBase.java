3556ad67c24e51d30b15fe4719d761cf6fdafdef
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-25 15:30:47 +0100

    Remove unused classes from the previous project

--- src/main/java/com/codecool/seasonalproductdiscounter/ui/factory/UiFactoryBase.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.seasonalproductdiscounter.ui.factory;
-
-import com.codecool.seasonalproductdiscounter.ui.UiBase;
-
-public abstract class UiFactoryBase {
-    public abstract String getUiName();
-    public abstract UiBase create();
-}
-
